4e1f5024145cda351e31c985e81d29e4c3231daf
Purnabiswal <purnabiswal824@gmail.com>
2026-07-17 15:37:12 +0530

    initial commit

---
+++ 1-Intro/script.js
@@ -0,0 +1,24 @@
+alert("Hellow World");
+
+console.log('Code is running');
+
+var a = prompt('Enter your number')
+
+console.log(a);
+
+
+var isTrue=confirm('Are you sure you want to leave this page and blast your computer ')
+
+if(isTrue){
+    console.log('Your computer is blasting');
+    
+}else{
+    console.log('Your computer is not blasting');
+    
+}
+
+
+document.title='Changed Title'
+
+
+document.body.style.backgroundColor='cyan'

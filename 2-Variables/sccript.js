3530eae395bfb32ea388b6446713d65d2940e2d9
Purnabiswal <purnabiswal824@gmail.com>
2026-07-18 14:09:30 +0530

    Objects & Variables Added

---
+++ 2-Variables/sccript.js
@@ -0,0 +1,59 @@
+console.log('Hello World');
+
+
+var a=5
+var b=6
+var c='Siba'
+
+var _a='Purna'
+var $a="Also Purna"
+
+// var 55a='Rohan' Not Allowed
+
+// console.log(typeof a, typeof b, typeof c);
+
+
+// console.log(a+b);
+
+
+// const a1=6
+// a1=a1+1 //Not allowed
+
+
+{
+    let a=66
+    // console.log(a);
+    
+}
+
+
+// console.log(a);
+
+
+
+
+let x='Hellow World'
+let y=22
+let z=3.55
+const p=true
+let q=undefined
+let r=null
+
+console.log(x, y, z, p, q, r);
+console.log(typeof x, typeof y, typeof z, typeof p, typeof q, typeof r);
+
+
+let o={
+    name: "Purna",
+    'Job code': 5600
+}
+
+console.log(o);
+
+o.salary='100crores'
+
+console.log(o);
+
+o.salary='500 crore'
+
+console.log(o);

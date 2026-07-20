733ec65547b21e2ef2cbb5ccccd0648946106524
Purnabiswal <purnabiswal824@gmail.com>
2026-07-20 11:39:43 +0530

    Conditionals Added

---
+++ 3-Conditionals/index.js
@@ -0,0 +1,28 @@
+// console.log("I'm conditional tutorial");
+
+let age=24
+// let grace=2
+
+// console.log(age + grace);
+// console.log(age - grace);
+// console.log(age * grace);
+// console.log(age / grace);
+// console.log(age ** grace); //Power
+
+
+if(age>18){
+    console.log("You can drive");
+    
+}else{
+    console.log('You cant drive');
+    
+}
+
+
+
+
+a=6
+b=8
+
+let c=(a>b)?(a-b):(b-a);
+console.log(c);

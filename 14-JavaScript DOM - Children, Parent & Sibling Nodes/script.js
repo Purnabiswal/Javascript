51dbe5067ef65ef26ac03f6bcdcb4b81d54ed744
Purnabiswal <purnabiswal824@gmail.com>
2026-07-22 00:15:18 +0530

    Javascript DOM continues

---
+++ 14-JavaScript DOM - Children, Parent & Sibling Nodes/script.js
@@ -0,0 +1,13 @@
+console.log("Hello");
+
+console.log(document.body.firstChild);
+
+// document.body.firstElementChild;
+// document.body.firstElementChild.childNodes;
+// document.body.firstElementChild.children;
+//document.body.firstElementChild.children[0].nextElementSibling
+
+
+
+
+
